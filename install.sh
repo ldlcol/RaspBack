f0a6691c3edf38e9312f35accf3967e5229b146b
KaO <52933590+painteau@users.noreply.github.com>
2023-01-01 11:39:37 +0100

    Update README and Install Script

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
+# Installing ffmpeg and git
 apt update -y && apt full-upgrade -y && apt autoremove -y && apt autoclean -y
 apt install -y git ffmpeg
+
+# Cloning repo into /home/pi/RaspBack/
 cd /home/pi/ && git clone https://github.com/painteau/RaspBack
-cp /home/pi/RaspBack/main.conf.example /home/pi/RaspBack/main.conf
 chmod +x /home/pi/RaspBack/stream.sh
+
+# Making new config file, please edit the config files to match your settings
+cp /home/pi/RaspBack/main.conf.example /home/pi/RaspBack/main.conf
+
+# Setting up the script at launch
 echo "bash /home/pi/RaspBack/stream.sh" >> /etc/rc.local
