aeed343cb6f406421cd151a5938accfe094590d1
KaO <thierryoliv@gmail.com>
2023-01-10 15:05:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt update -y && apt full-upgrade -y && apt autoremove -y && apt autoclean -y
 apt install -y git ffmpeg
 
 # Cloning repo into /home/pi/RaspBack/
-cd /home/pi/ && git clone https://github.com/painteau/RaspBack
+cd /home/pi/ && git clone https://github.com/ldlcol/RaspBack
 chmod +x /home/pi/RaspBack/stream.sh
 
 # Making new config file, please edit the config files to match your settings
