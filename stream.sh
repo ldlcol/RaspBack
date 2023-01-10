3a2e7e15cfaec0ebe79ff6c1366e500aa16b3cab
KaO <thierryoliv@gmail.com>
2023-01-10 14:54:12 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -8,15 +8,15 @@ source /home/pi/RaspBack/lights.conf
 while true
 do
         #LAUNCHING STREAM
-        setLightState $GREEN $OFF
-        setLightState $RED $ON
+        # setLightState $GREEN $OFF
+        # setLightState $RED $ON
         sleep 2
         # pkill -9 -f ffmpeg
-        setLightState $RED $OFF
-        setLightState $GREEN $ON
+        # setLightState $RED $OFF
+        # setLightState $GREEN $ON
         ffmpeg \
-                -re -f v4l2 -s $videosize -input_format $videocardtype -framerate $framerate -i $videocard \
-                -f alsa -ac $audiochannels -i $audiocard -c:a $audioencoder -b:a $bitrateaudio -ar $sampling \
+                -re -f v4l2 -s $videosize -input_format $videocardtype -framerate $framerate -i $videocard -thread_queue_size 384 \
+                -f alsa -ac $audiochannels -i $audiocard -c:a $audioencoder -b:a $bitrateaudio -ar $sampling -thread_queue_size 4096 \
                 -vf format=$outputformat -c:v $encoder -b:v $bitrate -preset $preset -f flv -drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 1 $streamkey
 done
 
